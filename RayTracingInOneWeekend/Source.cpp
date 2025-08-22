435e0ed6b5f31f84d62f2a376c54aadf8118b2ce
Emily Fletcher <emilyfletcher099@gmail.com>
2025-08-23 00:56:28 +0100

    Chapter 3 finished

--- RayTracingInOneWeekend/Source.cpp
+++ RayTracingInOneWeekend/Source.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main()
@@ -14,15 +17,12 @@ int main()
 		std::clog << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++)
 		{
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.0;
-
-			int ir = int(255.999 * r);
-			int ig = int(255.999 * g);
-			int ib = int(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			auto pixel_color = color(
+				double(i) / (image_width - 1),  // Normalized x coordinate
+				double(j) / (image_height - 1), // Normalized y coordinate
+				0								// Fixed blue component	
+			);                            
+			write_color(std::cout, pixel_color);
 		}
 	}
 	std::clog << "\nDone.\n";
