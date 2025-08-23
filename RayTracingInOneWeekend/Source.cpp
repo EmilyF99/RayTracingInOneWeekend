2a1b98e9ff6e1fd00e2201b7e3401f5ad3447264
Emily Fletcher <emilyfletcher099@gmail.com>
2025-08-23 18:19:14 +0100

    Red sphere rendered

--- RayTracingInOneWeekend/Source.cpp
+++ RayTracingInOneWeekend/Source.cpp
@@ -4,9 +4,24 @@
 
 #include <iostream>
 
+// Detection between a ray and a basic sphere
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminent = b * b - 4 * a * c; 
+	return (discriminent >= 0);
+}
+
 // Blue to white Lerp
 color ray_color(const ray& r)
 {
+	// If we see the sphere turn that area red
+	if (hit_sphere(point3(0, 0, 1), 0.5, r))
+		return color(1, 0, 0);
+
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
